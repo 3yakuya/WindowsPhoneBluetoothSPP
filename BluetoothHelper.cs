76ea9f798da6fa8207b6f2b9ec33a5766651fe7c
byakuya6 <inf.byakuya@gmail.com>
2015-05-20 03:26:29 +0200

    Added code, provided basic instructions in README.

---
+++ BluetoothHelper.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Phone.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Proximity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace Streamoli.Utilities
+{
+    class BluetoothHelper
+    {
+        private static BluetoothHelper instance = null;
+        private string deviceName;
+        private int rfcommPortNumber;
+        private DataWriter outputWriter;
+
+        public static async Task<BluetoothHelper> GetInstance(string deviceName, int rfcommPortNumber)
+        {
+            if (instance == null)
+            {
+                instance = new BluetoothHelper(deviceName, rfcommPortNumber);
+                instance.outputWriter = await instance.GetOutputDataWriter(deviceName, rfcommPortNumber);
+            }
+
+            return instance;
+        }
+
+        private BluetoothHelper(string deviceName, int rfcommPortNumber)
+        {
+            this.deviceName = deviceName;
+            this.rfcommPortNumber = rfcommPortNumber;
+        }
+
+        public static void ShowBluetoothSettings()
+        {
+            ConnectionSettingsTask connectionSettingsTask = new ConnectionSettingsTask();
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.Bluetooth;
+            connectionSettingsTask.Show();
+        }
+
+        public async Task WriteData(string data)
+        {
+            byte[] byteData = Encoding.UTF8.GetBytes(data.ToCharArray());
+            this.outputWriter.WriteBytes(byteData);
+            await this.outputWriter.StoreAsync();
+            await this.outputWriter.FlushAsync();
+        }
+
+        public void Disconnect()
+        {
+            this.outputWriter.DetachStream();
+            this.outputWriter.Dispose();
+        }
+
+        private async Task<DataWriter> GetOutputDataWriter(string name, int rfcommPortNumber)
+        {
+            PeerInformation peerInformation = await GetPeerInformation(name);
+            StreamSocket connectedSocket = await Connect(peerInformation, rfcommPortNumber);
+            DataWriter streamWriter = new DataWriter(connectedSocket.OutputStream);
+            streamWriter.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+            return streamWriter;
+        }
+
+        private async Task<PeerInformation> GetPeerInformation(string name)
+        {
+            PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+            var allPeers = await PeerFinder.FindAllPeersAsync();
+            var filteredPeers = allPeers.Where(x => x.DisplayName == name).ToList();
+            return filteredPeers[0];
+        }
+
+        private async Task<StreamSocket> Connect(PeerInformation peerInformation, int rfcommPortNumber)
+        {
+            StreamSocket socket = GetStreamSocket();
+            await socket.ConnectAsync(peerInformation.HostName, rfcommPortNumber.ToString());
+            return socket;
+        }
+
+        private StreamSocket GetStreamSocket()
+        {
+            return new StreamSocket();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # WindowsPhoneBluetoothSPP
 Simple class to help WindowsPhone 8.1 devices send data to bluetooth devices.
+
+This is a simple class comming from a project I am happy to be part of: Streamoli.
+It is a singleton, with two public working methods, designed to help some Windows Phone 8.1 code:
++ ShowBluetoothSettings() which launches bluetooth settings window (allowing to pair phone with desired device)
++ SendData(string), which sends the argument string to the device (serial-port emulation.)
+
+When getting an instance, you should pass the name of the device (as seen in the bluetooth settings window) and port (it is often 1.) An attempt to connect with the device will be made when creating the instance. The code also assumes there is just one device with given name available around.
+
+This is working code. However, it is meant to be a reference for using Windows Phone with bluetooth devices, rather than ready-to-go code. Before using it in your project, make sure you understand what is happening and whether this is what you want to be happening.
+
+3yakuya, Stream Team, 2015
